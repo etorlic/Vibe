24e78d6289ecdbe4d1251c5bb93305b1c3fb5e08
etorlic <emin.torlic@icloud.com>
2021-12-03 17:37:13 -0800

    Updated site

--- vibe/src/App.js
+++ vibe/src/App.js
@@ -1,13 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { SignIn, SignOut, useAuthentication } from "./services/authService.js";
 
 export default function App() {
+  const user = useAuthentication();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Vibe</p>
+        <p>VIBE</p>
       </header>
+      <body>
+        <div className="Sign-in-out-button">
+          {!user ? <SignIn /> : <SignOut />}
+        </div>
+      </body>
     </div>
   );
 }
