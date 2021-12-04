a989aca5688c8bd03b766b5d7787b8aee6c4fd56
etorlic <emin.torlic@icloud.com>
2021-12-03 16:19:01 -0800

    Added files

---
+++ vibe/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLL12ur18HXmUBEM3p_b55ku76In35UJM",
+  authDomain: "vibe-b81c4.firebaseapp.com",
+  projectId: "vibe-b81c4",
+  storageBucket: "vibe-b81c4.appspot.com",
+  messagingSenderId: "445674678160",
+  appId: "1:445674678160:web:6a25348daf912d6407b515",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
