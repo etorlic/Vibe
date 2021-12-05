9ae3af3abbcacf0a0273a7b0390594f741bd3b41
claytoncb <cbruce4@lion.lmu.edu>
2021-12-04 19:07:29 -0800

    added comments to CreatePlaylist.js

--- src/Components/CreatePlaylist.js
+++ src/Components/CreatePlaylist.js
@@ -1,6 +1,11 @@
 export default function CreatePlaylist() {
+    //will have a few sliders to filter songs
+
+    //will have a search bar so that you
+    //can also filter using a selected song
+    
     return (
-        <div>
+        <div className="create">
             hello
         </div>
     );
