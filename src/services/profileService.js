981e0c695e2de3cd3032aadfbf31e365c875e47f
claytoncb <70668150+claytoncb@users.noreply.github.com>
2021-12-14 17:49:40 -0800

    Update profileService.js

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,11 +1,12 @@
 import { collection, setDoc, updateDoc, getDoc, deleteDoc, doc } from "firebase/firestore"
 import { db } from "../firebaseConfig"
 
-export async function AddUserInfo(username, streamingService, userId) {
+export async function AddUserInfo(username, streamingService, currentGenre, userId) {
   try {
     await setDoc(doc(db, "Users", userId), {
       Streaming: streamingService,
       Username: username,
+      Genre: currentGenre
     })
     console.log("Document written with ID: ", userId)
   } catch (e) {
@@ -13,13 +14,14 @@ export async function AddUserInfo(username, streamingService, userId) {
   }
 }
 
-export async function EditUserInfo(username, streamingService, userId) {
+export async function EditUserInfo(username, streamingService, currentGenre, userId) {
   try {
     const userRef = doc(db, "Users", userId)
     console.log(streamingService)
     await updateDoc(userRef, {
       Username: username,
       Streaming: streamingService,
+      Genre: currentGenre
     })
     console.log("Document written with ID: ", userId)
   } catch (e) {
