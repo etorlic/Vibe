ad6cbb1622e2839a3724decd0b2e430a7d987a51
claytoncb <70668150+claytoncb@users.noreply.github.com>
2021-12-15 13:42:41 -0800

    Update DisplayMovie.js

--- src/Components/DisplayMovie.js
+++ src/Components/DisplayMovie.js
@@ -8,7 +8,6 @@ import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 import TableCell from "@mui/material/TableCell"
 import TableBody from "@mui/material/TableBody"
-import EditGenre from "./EditGenre"
 
 
 
